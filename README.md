8011926a0f9facb1159e447e8d9453b87156817b
roney.dsilva <roney.dsilva@cdmx.in>
2023-08-26 17:48:16 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#### Created and Maintained by: Roney Dsilva
+
+# Select2 Extension Module for Select Module
+
+The Select2 extension module enhances the functionality of the Select module by providing advanced options for selecting and displaying data. This README provides an overview of the available attributes and configurations for the Select2 component.
+
+## Table of Contents
+- [Attributes](#attributes)
+  - [Select2 Placeholder](#select2placeholder)
+  - [Select2 Theme](#select2theme)
+
+## Attributes
+
+### `select2Placeholder`
+- **Default Value:** Null
+- **Description:** This attribute enables the use of a placeholder in the Select2 component, allowing you to display a hint or prompt text when no option is selected.
+
+
+### `select2Theme`
+- **Description:** This attribute allows you to choose a theme for the Select2 component, giving you control over its visual appearance.
+- **Values:**
+- Default: Default theme
+- Bootstrap-5: Bootstrap 5 theme
---
+++ dmx-select2.js
@@ -0,0 +1,97 @@
+dmx.Component("select2", {
+  extends: "form-element",
+  initialData: { 
+      selectedIndex: -1, 
+      selectedValue: "", 
+      selectedText: "" 
+  },
+  tag: "select",
+  attributes: {
+    options: { type: Array, default: [] },
+    optionText: { type: String, default: "$value" },
+    optionValue: { type: String, default: "$value" },
+    field_theme: { type: String, default: "" },
+  },
+  methods: {
+    setValue: function (t, e) {
+      dmx.array(this.$node.options).forEach(function (n) {
+        (n.selected = n.value == t), e && (n.defaultSelected = n.selected);
+      });
+      this.renderSelect();
+    },
+  },
+  renderSelect: function () {
+    $("#" + this.$node.id).data("placeholder", this.$node.dataset.placeholder);
+    var dropdownParent = null;
+    // Check if the parent of the element is a modal
+    if ($("#" + this.$node.id).closest(".modal").length > 0) {
+        dropdownParent = $("#" + this.$node.id).closest(".modal");
+    }
+    $("#" + this.$node.id).select2({
+        theme: this.props.field_theme,
+        dropdownParent: dropdownParent,
+    });
+  },
+  render: function (node) {
+      (this.options = []),
+      this.props.value ? (this.updateValue = !0) : (this.props.value = this.$node.value),
+      dmx.BaseComponent.prototype.render.call(this, node),
+      (this.$node.disabled = this.props.disabled),
+      this.$node.addEventListener("change", this.updateData.bind(this)),
+      this.$node.addEventListener("invalid", this.updateData.bind(this)),
+      this.$node.addEventListener("focus", this.updateData.bind(this)),
+      this.$node.addEventListener("blur", this.updateData.bind(this)),
+      this.renderOptions(),
+      this.updateData();
+  },
+  update: function (t, e) {
+      e.has("options") && (this.renderOptions(), (this.updateValue = !0)),
+      e.has("value") && (this.updateValue = !0),
+      t.disabled != this.props.disabled && (this.$node.disabled = this.props.disabled),
+      this.updateData()
+      this.renderSelect();
+  },
+  updated: function () {
+    this.updateValue &&
+      ((this.updateValue = !1),
+      this.setValue(this.props.value, !0),
+      this.updateData());
+  },
+  updateData: function (t) {
+    dmx.Component("form-element").prototype.updateData.call(this, t);
+    var e = this.$node.selectedIndex;
+    this.set("selectedIndex", e),
+      this.set(
+        "selectedValue",
+        (this.$node.options[e] && this.$node.options[e].value) || ""
+      ),
+      this.set(
+        "selectedText",
+        (this.$node.options[e] && this.$node.options[e].text) || ""
+      );
+  },
+  renderOptions: function () {
+    this.options.splice(0).forEach(function (t) {
+      dmx.dom.remove(t);
+    }),
+      this.$node.dataset.placeholder
+        ? this.options.push(
+            this.$node.appendChild(document.createElement("option"))
+          )
+        : null;
+    dmx.repeatItems(this.props.options).forEach(function (t) {
+      "object" != typeof t && (t = { $value: t });
+      var e = document.createElement("option");
+      (e.value = Array.isArray(this.props.options)
+        ? dmx.parse(this.props.optionValue, dmx.DataScope(t, this))
+        : t.$key),
+        (e.innerText = dmx.parse(
+          this.props.optionText,
+          dmx.DataScope(t, this)
+        )),
+        this.options.push(this.$node.appendChild(e));
+    }, this);
+  }
+});
+
+//Created and Maintained by Roney Dsilva
